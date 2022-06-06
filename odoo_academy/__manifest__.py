6c3b0b074b8ca6f57aa92f41f761cb439159b803
Lars Liedtke <lal@solute.de>
2022-06-06 20:46:03 +0200

    [ADD]: Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Odoo Academy",
+    "summary": """Academy App to manage training""",
+    "description": """
+        Academy module to manage training:
+        - Courses
+        - Sessions
+        - Attendees
+    """,
+    "author": "Lars Liedtke",
+    "website": "https://www.odoo.com",
+    "category": "Training",
+    "version": "0.1",
+    "depends": ["base"],
+    "data": [
+        
+    ],
+    "demo": [
+        
+    ]
+}
