038bbb1a5e063c5c8b81dfd6a786d91aa98ffd79
Lars Liedtke <lal@solute.de>
2022-06-15 19:42:19 +0200

    add sale_order model

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -11,7 +11,7 @@
     "website": "https://www.odoo.com",
     "category": "Training",
     "version": "0.1",
-    "depends": ["base"],
+    "depends": ["sale"],
     "data": [
         "security/academy_security.xml",
         "security/ir.model.access.csv",
