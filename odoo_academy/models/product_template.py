34fc23d27625659b47f1b28d3f0c950adf36d690
Lars Liedtke <lal@solute.de>
2022-06-15 20:48:55 +0200

    add product template overwrite

---
+++ odoo_academy/models/product_template.py
@@ -0,0 +1,13 @@
+
+from odoo import models, fields, api
+
+
+class ProductTemplate(models.Model):
+
+    _inherit = "product.template"
+
+    is_session_product = fields.Boolean(
+        string="Use as Session Product",
+        help="Check this Box to use as a Product for Session Fee",
+        default=False
+    )
